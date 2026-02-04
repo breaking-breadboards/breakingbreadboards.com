93cf6bbd72971045bd850e7dbafd8e775acc5a18
Nikhil Dabas <nikhil@nikhildabas.com>
2026-02-04 16:13:11 +0000

    Publish to root for CloudFlare pages deployment

--- eleventy.config.js
+++ eleventy.config.js
@@ -31,6 +31,6 @@ export const config = {
     input: "content",          // default: "."
     includes: "../_includes",  // default: "_includes" (`input` relative)
     data: "../_data",          // default: "_data" (`input` relative)
-    output: "_site"
+    output: "."                // default: "_site"
   }
 };
